cbb01a3b638795a77029c906c766d2b28b845f21
Danial-Yazdani <86047465+Danial-Yazdani@users.noreply.github.com>
2024-02-27 11:10:07 +1100

    Minor update

--- Python/GNBG_instances.py
+++ Python/GNBG_instances.py
@@ -73,10 +73,6 @@ folder_path = os.path.join(current_dir)
 # Initialization
 ProblemIndex = 1  # Choose a problem instance range from f1 to f24
 
-# Check if the chosen problem index is within the valid range
-if ProblemIndex < 1 or ProblemIndex > 24:
-    raise ValueError("Invalid problem index. Please choose a number from 1 to 24.")
-
 # Preparation and loading of the GNBG parameters based on the chosen problem instance
 if 1 <= ProblemIndex <= 24:
     filename = f'f{ProblemIndex}.mat'
